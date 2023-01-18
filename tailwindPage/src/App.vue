fbc976cc6ff5c88f419afe24f7eb0f38380c0412
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-01-18 17:43:58 -0300

    tailwUpdate

--- tailwindPage/src/App.vue
+++ tailwindPage/src/App.vue
@@ -1,6 +1,63 @@
 <template>
-  <div>
-    
+  <div class="flex h-screen">
+    <aside class="columns-1">
+      <div class="flex flex-row">
+        <div>
+          logo
+        </div>
+      </div>
+      <div class="row">
+        <div class="flex flex-row">
+          <div class="basis-1/4">Profile Foto</div>
+          <div class="basis-1/2">Nome do cabaço</div>
+          <div class="basis-1/4">seta</div>
+        </div>
+      </div>
+
+      <div>
+        <ul>
+          <li>ola</li>
+          <li>ola</li>
+        </ul>
+      </div>
+
+      <hr>
+
+      <div>
+        <ul>
+          <li>ola</li>
+          <li>ola</li>
+        </ul>
+      </div>
+
+      <hr>
+
+      <div>
+        <ul>
+          <li>ola</li>
+          <li>ola</li>
+        </ul>
+      </div>
+
+      <hr>
+
+      <div>
+        <ul>
+          <li>ola</li>
+          <li>ola</li>
+        </ul>
+      </div>
+
+      <div>
+        <ul>
+          <li>ola</li>
+          <li>ola</li>
+        </ul>
+      </div>
+    </aside>
+    <div class="columns-1">
+      diasdjasd
+    </div>
   </div>
 </template>
 
@@ -12,6 +69,4 @@ export default {
 
 <style scoped>
 @import "./assets/main.css";
-
-
 </style>
