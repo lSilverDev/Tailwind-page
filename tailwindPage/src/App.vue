ba448a58dfd714a5dd9175797db38dc31460a3e9
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-01-18 16:03:47 -0300

    first commit

---
+++ tailwindPage/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
