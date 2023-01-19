76c04450632f04b5f5c0a9688f3ec3173409771b
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-01-19 08:45:44 -0300

    ring

--- tailwindPage/tailwind.config.js
+++ tailwindPage/tailwind.config.js
@@ -11,7 +11,31 @@ module.exports = {
       lg: '976px',
       xl: '1440px',
     },
-    extend: {},
+    extend: {
+      colors: {
+        azul: {
+            claro: '#C5DFFF',
+            escuro: '#061E3C',
+            hover: '#1057B0'
+        },
+      },
+      fontFamily: {
+          inter: ['Inter', 'sans-serif']    
+      },
+      keyframes: {
+        sino_kf: {
+            '0%, 100%': {
+                transform: 'rotate(-10deg)'
+            },
+            '50%': {
+                transform: 'rotate(10deg)'
+            }
+        }
+      },
+      animation: {
+          sino: 'sino_kf 0.31s ease-in-out infinite'
+      }
+    },
   },
   plugins: [],
 }
