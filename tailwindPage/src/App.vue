9d1c74a124dfa2ff588d7de04b6ad5e3122b89d9
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-01-21 11:59:16 -0300

    pattern and new fields

--- tailwindPage/src/App.vue
+++ tailwindPage/src/App.vue
@@ -1,50 +1,20 @@
 <template>
   <div class="flex h-screen bg-black-dark">
   <Sidebar/>
-    <main class="flex flex-col">
-      <div class="flex w-4/5 flex-row">
-        <div>
-          <span class="material-symbols-outlined">
-            menu
-          </span>
-        </div>
-        <div>
-          <img class="rounded-3xl" src="https://pbs.twimg.com/media/EbQ1oAHXsAIoHUy?format=jpg&name=small" alt="">
-        </div>
-      </div>
-      <div class="flex flex-row">
-        <div class="card">
-          upload or create video
-          <p>isjdijasidijsijdiasidisaidisdiasidiajdasddasds</p>
-          <div>
-            footer
-          </div>
-        </div>
-        <div>
-          4h
-        </div>
-        <div>
-          1,2m
-        </div>
-        <div>
-          9848
-        </div>
-      </div>
-      <div class="flex flex-row">
-
-      </div>
-    </main>
+  <Content/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Sidebar from "./components/Sidebar/Sidebar.vue";
+import Content from "./components/Content/Content.vue";
 
 export default defineComponent({
   name: 'App',
   components:{
     Sidebar,
+    Content
   }
 });
 </script>
---
+++ tailwindPage/src/components/Content/Content.vue
@@ -0,0 +1,76 @@
+<template>
+<main class="flex flex-col">
+      <div class="flex w-4/5 flex-row">
+        <div>
+          <span class="material-symbols-outlined">
+            menu
+          </span>
+        </div>
+        <div>
+          <img class="rounded-3xl" src="https://pbs.twimg.com/media/EbQ1oAHXsAIoHUy?format=jpg&name=small" alt="">
+        </div>
+      </div>
+      <div class="flex flex-row">
+        <div class="rounded-lg p-8 border-2 border-white border-solid">
+          upload or create video
+          <p>isjdijasidijsijdiasidisaidisdiasidiajdasddasds</p>
+          <div>
+            footer
+          </div>
+        </div>
+        <div class="rounded-lg p-8 border-2 border-white border-solid">
+          4h
+        </div>
+        <div class="rounded-lg p-8 border-2 border-white border-solid">
+          1,2m
+        </div>
+        <div class="rounded-lg p-8 border-2 border-white border-solid">
+          9848
+        </div>
+      </div>
+      <div class="flex flex-row">
+        <div class="rounded-lg p-8 border-2 border-white border-solid">
+          New Subscribers
+          <p>isjdijasidijsijdiasidisaidisdiasidiajdasddasds</p>
+          <div>
+            footer
+          </div>
+        </div>
+        <div class="rounded-lg p-8 border-2 border-white border-solid">
+          New comments
+          <div>
+            footer card
+          </div>
+        </div>
+        <div class="rounded-lg p-8 border-2 border-white border-solid">
+          Account Verification
+          <p>isjdijasidijsijdiasidisaidisdiasidiajdasddasds</p>
+          <div>
+            <button>Footer</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>Most Popular Video</p>
+      </div>
+      <div class="flex flex-row">
+        <div class="rounded-lg p-8 border-2 border-white border-solid">
+          Video
+          <p>isjdijasidijsijdiasidisaidisdiasidiajdasddasds</p>
+          <div>
+            footer
+          </div>
+        </div>
+      </div>
+    </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'Content',
+});
+</script>
+
+<style></style>
