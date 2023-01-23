8d93bbc4b570fab6a5a4a2742e9bad8ded614887
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-01-23 07:27:59 -0300

    icons and text

--- tailwindPage/src/components/Content/Content.vue
+++ tailwindPage/src/components/Content/Content.vue
@@ -3,8 +3,9 @@
       <ContentHeader/>
       <ContentInfo/>
       <ContentAccount/>
-      <div>
-        <p>Most Popular Video</p>
+      <div class="flex flex-row">
+        <VideoCameraIcon class="w-6 h-6 stroke-pink-light"/>
+        <p class="px-2">Most Popular Video</p>
       </div>
       <div class="flex flex-row p-8">
         <div class="rounded-lg p-8 border-2 border-white border-solid">
@@ -23,13 +24,15 @@ import { defineComponent } from "vue";
 import ContentHeader from "./ContentHeader.vue";
 import ContentInfo from "./ContentInfo.vue";
 import ContentAccount from "./ContentAccount.vue";
+import { VideoCameraIcon } from '@heroicons/vue/24/outline';
 
 export default defineComponent({
   name: 'Content',
   components: {
     ContentHeader,
     ContentInfo,
-    ContentAccount
+    ContentAccount,
+    VideoCameraIcon
   }
 });
 </script>
--- tailwindPage/src/components/Content/ContentAccount.vue
+++ tailwindPage/src/components/Content/ContentAccount.vue
@@ -1,32 +1,63 @@
 <template>
       <div class="flex flex-row p-8">
         <div class="rounded-lg p-8 border-2 border-white border-solid">
-          New Subscribers
-          <p>isjdijasidijsijdiasidisaidisdiasidiajdasddasds</p>
+          <div class="flex flex-row">
+            <FireIcon class="w-6 h-6 stroke-pink-light"/>
+            <p class="px-2">New Subscribers</p>
+          </div>
           <div>
-            footer
+            <p>Fotos</p>
+          </div>
+          <div class="flex flex-row">
+            <p>20</p>
+            <p class="px-2">Last 90 days</p>
           </div>
         </div>
         <div class="rounded-lg p-8 border-2 border-white border-solid">
-          New comments
-          <div>
-            footer card
+          <div class="flex flex-row">
+            <ChatBubbleLeftEllipsisIcon class="w-6 h-6 stroke-pink-light"/>
+            <p class="px-2">New comments</p>
+          </div>
+          <div class="flex flex-row">
+            <div class="flex flex-col">
+              <div>img</div>
+            </div>
+            <div class="flex flex-col px-2">
+              <div class="flex flex-row">
+                <p>Name</p>
+              </div>
+              <div class="flex flex-row">
+                <p>Comment</p>
+              </div>
+            </div>
+            
           </div>
         </div>
         <div class="rounded-lg p-8 border-2 border-white border-solid">
-          Account Verification
-          <p>isjdijasidijsijdiasidisaidisdiasidiajdasddasds</p>
-          <div>
-            <button>Footer</button>
+          <div class="flex flex-col">
+            <p>Account Verification</p>
+            <p>You have already passed all the conditions for Verification of the account.</p>
+            <div class="py-2">
+              <button class="rounded-lg border-1 bg-pink-light p-1 px-6">Let's go'</button>
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <p>Selo</p>
           </div>
         </div>
       </div>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from "vue";
+import { defineComponent } from "vue";
+import { FireIcon } from '@heroicons/vue/24/outline';
+import { ChatBubbleLeftEllipsisIcon } from '@heroicons/vue/24/outline';
 
-    export default defineComponent({
-        name: 'ContentAccount',
-    });
+export default defineComponent({
+    name: 'ContentAccount',
+    components: {
+      FireIcon,
+      ChatBubbleLeftEllipsisIcon
+    }
+});
 </script>
