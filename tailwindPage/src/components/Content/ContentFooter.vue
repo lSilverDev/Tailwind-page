0c46b4ebcf024477d610b91b82a04fa512ae84a5
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-01-23 12:32:09 -0300

    popular video done

--- tailwindPage/src/components/Content/ContentFooter.vue
+++ tailwindPage/src/components/Content/ContentFooter.vue
@@ -1,48 +1,54 @@
 <template>
-    <div class="flex flex-row p-8 rounded-lg p-8 border-2 border-white border-solid">
-        <div class="flex flex-row">
-            <VideoCameraIcon class="w-6 h-6 stroke-pink-light "/>
+    <div class="flex flex-row">
+            <VideoCameraIcon class="w-6 h-6 stroke-pink-light"/>
             <p class="px-2">Most Popular Video</p>
         </div>
+    <div class="flex flex-row p-8 rounded-lg p-8 border-2 border-white border-solid">
         <div class="flex flex-row">
             <div class="flex flex-col">
-            <img class="rounded-3xl w-1/3" src="https://pbs.twimg.com/media/EbQ1oAHXsAIoHUy?format=jpg&name=small" alt=""/>
+                <img class="rounded-3xl" src="https://pbs.twimg.com/media/EbQ1oAHXsAIoHUy?format=jpg&name=small" alt=""/>
             </div>
             <div class="flex flex-col">
-            <p>Clash of clans village</p>
+                <p>Clash of clans village</p>
             </div>
             <div class="flex flex-col">
-            <div class="flex flex-row">
-                <p>curtir</p>
-                <p>curtir</p>
-                <p>curtir</p>
-            </div>
-            </div>
-            <div class="flex flex-col">
-            <div class="flex flex-row">
-                <p>reacao</p>
-                <p>reacao</p>
-                <p>reacao</p>
-            </div>
+                <div class="flex flex-row">
+                    <HeartIcon class="w-6 h-6 stroke-pink-light"/><p class="px-2">230</p>
+                    <ChatBubbleLeftEllipsisIcon class="w-6 h-6 stroke-pink-light"/><p class="px-2">230</p>
+                    <BookmarkIcon class="w-6 h-6 stroke-pink-light"/>
+                </div>
             </div>
             <div class="flex flex-col">
-            <div class="flex flex-row">
-                <p>... Icon</p>
+                <div class="flex flex-row">
+                    <p>reacao</p>
+                    <p>reacao</p>
+                    <p>reacao</p>
+                </div>
             </div>
+            <div class="flex flex-col">
+                <div class="flex flex-row">
+                    <p>... Icon</p>
+                </div>
             </div>
         </div>
     </div>
-    </template>
-    
-    <script lang="ts">
-    import { defineComponent } from "vue";
-    import { VideoCameraIcon } from '@heroicons/vue/24/outline';
-    
-    export default defineComponent({
-      name: 'ContentFooter',
-      components: {
-        VideoCameraIcon
-      }
-    });
-    </script>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { VideoCameraIcon } from '@heroicons/vue/24/outline';
+import { HeartIcon } from '@heroicons/vue/24/outline';
+import { BookmarkIcon } from '@heroicons/vue/24/outline';
+import { ChatBubbleLeftEllipsisIcon } from '@heroicons/vue/24/outline';
+
+export default defineComponent({
+    name: 'ContentFooter',
+    components: {
+    VideoCameraIcon,
+    BookmarkIcon,
+    ChatBubbleLeftEllipsisIcon,
+    HeartIcon
+    }
+});
+</script>
     
