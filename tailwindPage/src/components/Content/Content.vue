80153f6bca67386972121e132df3b012f0748fc7
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-01-23 07:38:15 -0300

    contentFooter

--- tailwindPage/src/components/Content/Content.vue
+++ tailwindPage/src/components/Content/Content.vue
@@ -1,22 +1,10 @@
 <template>
-<main class="flex flex-col">
+  <main class="flex flex-col">
       <ContentHeader/>
       <ContentInfo/>
       <ContentAccount/>
-      <div class="flex flex-row">
-        <VideoCameraIcon class="w-6 h-6 stroke-pink-light"/>
-        <p class="px-2">Most Popular Video</p>
-      </div>
-      <div class="flex flex-row p-8">
-        <div class="rounded-lg p-8 border-2 border-white border-solid">
-          Video
-          <p>isjdijasidijsijdiasidisaidisdiasidiajdasddasds</p>
-          <div>
-            footer
-          </div>
-        </div>
-      </div>
-    </main>
+      <ContentFooter/>
+  </main>
 </template>
 
 <script lang="ts">
@@ -24,7 +12,7 @@ import { defineComponent } from "vue";
 import ContentHeader from "./ContentHeader.vue";
 import ContentInfo from "./ContentInfo.vue";
 import ContentAccount from "./ContentAccount.vue";
-import { VideoCameraIcon } from '@heroicons/vue/24/outline';
+import ContentFooter from "./ContentFooter.vue";
 
 export default defineComponent({
   name: 'Content',
@@ -32,7 +20,7 @@ export default defineComponent({
     ContentHeader,
     ContentInfo,
     ContentAccount,
-    VideoCameraIcon
+    ContentFooter
   }
 });
 </script>
---
+++ tailwindPage/src/components/Content/ContentFooter.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="flex flex-row p-8 rounded-lg p-8 border-2 border-white border-solid">
+        <div class="flex flex-row">
+            <VideoCameraIcon class="w-6 h-6 stroke-pink-light "/>
+            <p class="px-2">Most Popular Video</p>
+        </div>
+        <div class="flex flex-row">
+            <div class="flex flex-col">
+            <img class="rounded-3xl w-1/3" src="https://pbs.twimg.com/media/EbQ1oAHXsAIoHUy?format=jpg&name=small" alt=""/>
+            </div>
+            <div class="flex flex-col">
+            <p>Clash of clans village</p>
+            </div>
+            <div class="flex flex-col">
+            <div class="flex flex-row">
+                <p>curtir</p>
+                <p>curtir</p>
+                <p>curtir</p>
+            </div>
+            </div>
+            <div class="flex flex-col">
+            <div class="flex flex-row">
+                <p>reacao</p>
+                <p>reacao</p>
+                <p>reacao</p>
+            </div>
+            </div>
+            <div class="flex flex-col">
+            <div class="flex flex-row">
+                <p>... Icon</p>
+            </div>
+            </div>
+        </div>
+    </div>
+    </template>
+    
+    <script lang="ts">
+    import { defineComponent } from "vue";
+    import { VideoCameraIcon } from '@heroicons/vue/24/outline';
+    
+    export default defineComponent({
+      name: 'ContentFooter',
+      components: {
+        VideoCameraIcon
+      }
+    });
+    </script>
+    
